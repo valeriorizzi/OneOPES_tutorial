b008564bdf9bac4034cdb706e436352c3f635adb
valeriorizzi <valeriorizzi33@gmail.com>
2026-07-17 10:58:48 +0200

    Bugfixes

--- 1.2_IdealCV_multi/run_multi.sh
+++ 1.2_IdealCV_multi/run_multi.sh
@@ -26,9 +26,9 @@ for dir in "${folders[@]}"; do
     cd "$dir"
 
     (
-        python3 ../FES_from_Reweighting_multiT_funnel.py --sigma 0.05 --colvar COLVAR.* --cv phi --bin 150 --temp 300 --min -3 --max 2 --deltaFat -0.2 --skip 1000 --stride 500 --out fes_phi.dat;
+        python3 ../../FES_from_Reweighting_multiT_funnel.py --sigma 0.05 --colvar COLVAR.* --cv phi --bin 150 --temp 300 --min -3 --max 2 --deltaFat -0.2 --skip 1000 --stride 500 --out fes_phi.dat;
         grep ' DeltaF ' fes_phi_* | awk '{print $4}' > deltaFstride.dat
-        python3 ../FES_from_Reweighting_multiT_funnel.py --sigma 0.05 --colvar COLVAR.* --cv phi --bin 150 --temp 300 --min -3 --max 2 --deltaFat -0.2 --skip 1000 --out fes_blocks_phi.dat --blocks 3;
+        python3 ../../FES_from_Reweighting_multiT_funnel.py --sigma 0.05 --colvar COLVAR.* --cv phi --bin 150 --temp 300 --min -3 --max 2 --deltaFat -0.2 --skip 1000 --out fes_blocks_phi.dat --blocks 3;
     ) &
 
     file_path="${dir}/deltaFstride.dat"
--- 1.3_SuboptimalCV_multi/run_multi_50ns.sh
+++ 1.3_SuboptimalCV_multi/run_multi_50ns.sh
@@ -26,9 +26,9 @@ for dir in "${folders[@]}"; do
     cd "$dir"
 
     (
-        python3 ../FES_from_Reweighting_multiT_funnel.py --sigma 0.05 --colvar COLVAR.* --cv phi --bin 150 --temp 300 --min -3 --max 2 --deltaFat -0.2 --skip 5000 --stride 1000 --out fes_phi.dat;
+        python3 ../../FES_from_Reweighting_multiT_funnel.py --sigma 0.05 --colvar COLVAR.* --cv phi --bin 150 --temp 300 --min -3 --max 2 --deltaFat -0.2 --skip 5000 --stride 1000 --out fes_phi.dat;
         grep ' DeltaF ' fes_phi_* | awk '{print $4}' > deltaFstride.dat
-        python3 ../FES_from_Reweighting_multiT_funnel.py --sigma 0.05 --colvar COLVAR.* --cv phi --bin 150 --temp 300 --min -3 --max 2 --deltaFat -0.2 --skip 5000 --out fes_blocks_phi.dat --blocks 5;
+        python3 ../../FES_from_Reweighting_multiT_funnel.py --sigma 0.05 --colvar COLVAR.* --cv phi --bin 150 --temp 300 --min -3 --max 2 --deltaFat -0.2 --skip 5000 --out fes_blocks_phi.dat --blocks 5;
     ) &
 
     file_path="${dir}/deltaFstride.dat"
